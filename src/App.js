a65056f6a9a8269853575f339de418e382770e09
David Bui <tbui.vdev@gmail.com>
2021-12-10 20:54:10 -0600

    General layout frame

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import './App.css'
+import './App.scss'
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<nav className="navbar app">App Bar</nav>
+			<nav className="navbar board">Board Bar</nav>
+			<div className="board-columns">
+				<div className="column">
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							{/* <img
+							src="https://res.cloudinary.com/practicaldev/image/fetch/s--JIe3p0M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/093ewdrgyf1kedlhzs51.png"
+							alt="img-alt"
+						/> */}
+							title: Thien Bui
+						</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							{/* <img
+							src="https://res.cloudinary.com/practicaldev/image/fetch/s--JIe3p0M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/093ewdrgyf1kedlhzs51.png"
+							alt="img-alt"
+						/> */}
+							title: Thien Bui
+						</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							{/* <img
+							src="https://res.cloudinary.com/practicaldev/image/fetch/s--JIe3p0M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/093ewdrgyf1kedlhzs51.png"
+							alt="img-alt"
+						/> */}
+							title: Thien Bui
+						</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below</li>
+					</ul>{' '}
+					<footer>Add another card</footer>
+				</div>
+			</div>
 		</div>
 	)
 }
