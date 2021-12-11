291afa8a03fbb126348c082767678a98c3ef8f32
David Bui <tbui.vdev@gmail.com>
2021-12-11 15:53:35 -0600

    customize scroll bars

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
 					<header>Brainstorm</header>
 					<ul>
 						<li>
-							{/* <img
+							<img
 							src="https://res.cloudinary.com/practicaldev/image/fetch/s--JIe3p0M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/093ewdrgyf1kedlhzs51.png"
 							alt="img-alt"
-						/> */}
+						/>
 							title: Thien Bui
 						</li>
-						<li>Add what you like to work on below</li>
+						<li>Add what you like to work on below. Add what you like to work on below</li>
 						<li>Add what you like to work on below</li>
 						<li>Add what you like to work on below</li>
 						<li>Add what you like to work on below</li>
