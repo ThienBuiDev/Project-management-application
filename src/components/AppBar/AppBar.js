3c53a56fdaa9eb12097f0055fd0be8ea5dbb3b78
David Bui <tbui.vdev@gmail.com>
2021-12-12 10:50:48 -0600

    Restructure App.js into components

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,8 +1,10 @@
+
+
 import React from 'react'
 import './AppBar.scss'
 
 function AppBar() {
-	return <nav className="navbar app">App Bar</nav>
+	return <nav className="navbar-app">App Bar</nav>
 }
 
 export default AppBar
---
+++ src/components/Task/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Task.scss'
+
+function Task() {
+	return (
+		<li className='task-item'>
+			<img
+				src='https://res.cloudinary.com/practicaldev/image/fetch/s--JIe3p0M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/093ewdrgyf1kedlhzs51.png'
+				alt='img-alt'
+			/>
+			title: Thien Bui
+		</li>
+	)
+}
+
+export default Task
