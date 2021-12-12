a9233dae79b2c2e1c849db820a0c57593ca14607
David Bui <tbui.vdev@gmail.com>
2021-12-12 14:27:01 -0600

    Replace ctask with card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Column.scss'
-import Task from 'components/Task/Task'
+import Card from 'components/Card/Card'
 function Column() {
 	return (
 		<div className='column'>
 			<header>Brainstorm</header>
-			<ul className='task-list'>
-                <Task />
-				<li className='task-item'>
+			<ul className='card-list'>
+                <Card />
+				<li className='card-item'>
 					Add what you like to work on below. Add what you like to work on below
 				</li>
-				<li className='task-item'>Add what you like to work on below</li>
-				<li className='task-item'>Add what you like to work on below</li>
-				<li className='task-item'>Add what you like to work on below</li>
-				<li className='task-item'>Add what you like to work on below</li>
+				<li className='card-item'>Add what you like to work on below</li>
+				<li className='card-item'>Add what you like to work on below</li>
+				<li className='card-item'>Add what you like to work on below</li>
+				<li className='card-item'>Add what you like to work on below</li>
 			</ul>
 			<footer>Add another card</footer>
 		</div>
