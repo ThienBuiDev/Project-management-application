7e79e37a459825423f76a8337c8c7d1a9ac17d68
David Bui <tbui.vdev@gmail.com>
2021-12-16 18:42:29 -0600

    addUI components to update and delete columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Column.scss'
 import { Container, Draggable } from 'react-smooth-dnd'
-
+import { Dropdown, DropdownItem, DropdownButton } from 'react-bootstrap'
 import Card from 'components/Card/Card'
+import ConfirmModal from 'components/Common/ConfirmModal'
+import { Form } from 'react-bootstrap'
 function Column({ column, onCardDrop }) {
 	const cards = column.cards.sort(
 		(a, b) => column.cardOrder.indexOf(a.id) - column.cardOrder.indexOf(b.id)
 	)
 
+	const [showConfirmModal, setShowConfirmModal] = useState(false)
+
+	const toggleConfirmModal = () => setShowConfirmModal(!showConfirmModal)
+
+	const onConfirmModalAction = (type) => {
+		if (type === 'close') {
+			//remove column
+			toggleConfirmModal()
+		}
+		toggleConfirmModal()
+	}
 	return (
 		<div className='column'>
-			<header className='column-drag-handle'>{column.title}</header>
+			<header className='column-drag-handle'>
+				<div className='column-title'>
+					<Form.Control
+						size='sm'
+						type='text'
+						placeholder='Enter column title'
+						className='column-content-editable'
+						value={column.title}
+						// onChange={handleColumnTitleChange}
+						// onEnter={addNewColumSubmit}
+					/>
+				</div>
+				<div className='column-dropdown-actions'>
+					<Dropdown>
+						<Dropdown.Toggle id='dropdown-basic' className='dropdown-btn' />
+
+						<Dropdown.Menu>
+							<Dropdown.Item>Add card...</Dropdown.Item>
+							<Dropdown.Item onClick={toggleConfirmModal}>
+								Remove column...
+							</Dropdown.Item>
+							<Dropdown.Item>Copy list (in development)</Dropdown.Item>
+							<Dropdown.Item>Archive cards (in development)</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
+			</header>
 
 			<div className='card-list'>
 				<Container
@@ -49,6 +88,12 @@ function Column({ column, onCardDrop }) {
 					Add another card
 				</div>
 			</footer>
+			<ConfirmModal
+				show={showConfirmModal}
+				onAction={onConfirmModalAction}
+				title='Remove Column'
+				content={`Are you sure you want to remove <strong>${column.title}</strong> column? <br />This action is not reversible`}
+			/>
 		</div>
 	)
 }
